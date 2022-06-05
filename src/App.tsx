dcef03cd5f30965074080ab4de745530e69f90a6
taoufik ahjam <taoufikahjam@gmail.com>
2022-06-05 13:20:35 +0200

    initial project Pokemon, add class pokemon, add mock pokemon

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,13 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
-
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+import React, { useState } from 'react';
+import POKEMONS from './models/mock-pokemon';
+import Pokemon from './models/pokemon';
+  
+const App: React.FC = () => {
+  const [pokemons] =useState<Pokemon[]>(POKEMONS);
+    
+ return (
+  <h1>{pokemons.length} </h1>
+ )
 }
-
+  
 export default App;

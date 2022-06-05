e1a8d0d768a522933dafac629d1481f7ad3e74cb
taoufik ahjam <taoufikahjam@gmail.com>
2022-06-05 22:03:23 +0200

    factorisation the code

--- src/components/Pokemon-item.tsx
+++ src/components/Pokemon-item.tsx
@@ -1,6 +1,8 @@
 import React, {FunctionComponent, useState} from 'react'
+import formatDate from '../helpers/format-date';
 import Pokemon from '../models/pokemon';
 import './pokemon-item.css'
+import formatType from './../helpers/format-type';
 
 type props = {
     pokemon:Pokemon,
@@ -18,54 +20,7 @@ export const PokemonItem:FunctionComponent<props> = ({pokemon,borderColor='green
      setColor('#000');
  }
 
-const formatDate=(date:Date):string=> {
-    return `${date.getDate()}/${date.getMonth()+1}/${date.getFullYear()}`;
-}
 
-const formatType = (type: string): string => {
-    let color: string;
-   
-    switch (type) {
-      case 'Feu': 
-        color = 'red lighten-1'; 
-        break; 
-      case 'Eau': 
-        color = 'blue lighten-1'; 
-        break; 
-      case 'Plante': 
-        color = 'green lighten-1'; 
-        break; 
-      case 'Insecte': 
-        color = 'brown lighten-1'; 
-        break; 
-      case 'Normal': 
-        color = 'grey lighten-3'; 
-        break; 
-      case 'Vol': 
-        color = 'blue lighten-3'; 
-        break; 
-      case 'Poison': 
-        color = 'deep-purple accent-1'; 
-        break; 
-      case 'Fée': 
-        color = 'pink lighten-4'; 
-        break; 
-      case 'Psy': 
-        color = 'deep-purple darken-2'; 
-        break; 
-      case 'Electrik': 
-        color = 'lime accent-1'; 
-        break; 
-      case 'Combat': 
-        color = 'deep-orange'; 
-        break; 
-      default: 
-        color = 'grey'; 
-        break; 
-    }
-   
-    return `chip ${color}`;
-  }
 
 
 

a0a4b5cf596b0f6d6fdcd02177ed8e3d3a56c086
taoufik ahjam <taoufikahjam@gmail.com>
2022-06-05 15:41:39 +0200

    add materialize and pokemon card

--- src/App.tsx
+++ src/App.tsx
@@ -1,20 +1,39 @@
-import React, { FunctionComponent,useEffect,useState } from 'react';
+import React, { FunctionComponent, useEffect, useState } from 'react';
 import POKEMONS from './models/mock-pokemon';
 import Pokemon from './models/pokemon';
-  
+
 const App: FunctionComponent = () => {
-  const [pokemons,setPokemon] =useState<Pokemon[]>([]);
-    
+  const [pokemons, setPokemon] = useState<Pokemon[]>([]);
+  useEffect(() => {
+    setPokemon(POKEMONS);
+  }, []);
 
+  return (
+    <div>
+      <h1>Pokémons</h1>
+      <div className="container">
+        <div className="row">
+          {pokemons.map((pokemon) => (
+            <div className="col s6 m4" key={pokemon.id}>
+              <div className="card horizontal">
+                <div className="card-image">
+                  <img src={pokemon.picture} alt={pokemon.name} />
+                </div>
+                <div className="card-stacked">
+                  <div className="card-content">
+                    <p>{pokemon.name}</p>
+                    <p>
+                      <small>{pokemon.created.toString()}</small>
+                    </p>
+                  </div>
+                </div>
+              </div>
+            </div>
+          ))}
+        </div>
+      </div>
+    </div>
+  );
+};
 
-useEffect(() => {
-      setPokemon(POKEMONS);
-},[])
-
-
- return (
-  <h1>{pokemons.length} </h1>
- )
-}
-  
 export default App;

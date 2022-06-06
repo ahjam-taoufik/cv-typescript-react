385302395d49eaa8b1346d58fc6e74b4607e0c8e
taoufik ahjam <taoufikahjam@gmail.com>
2022-06-06 15:59:50 +0200

    add reviews value in form

--- src/components/pokemon-form.tsx
+++ src/components/pokemon-form.tsx
@@ -1,52 +1,110 @@
-import React, { FunctionComponent } from 'react';
+import React, { FunctionComponent, useState } from 'react';
 import formatType from '../helpers/format-type';
 import Pokemon from './../models/pokemon';
 
 type Props = {
-  pokemon: Pokemon
+  pokemon: Pokemon;
 };
-  
-const PokemonForm: FunctionComponent<Props> = ({pokemon}) => {
-  
+
+type Field = {
+  value?: any;
+  error?: string;
+  isValid?: boolean;
+};
+
+type Form = {
+  name: Field;
+  hp: Field;
+  cp: Field;
+  types: Field;
+};
+
+const PokemonForm: FunctionComponent<Props> = ({ pokemon }) => {
+  const [form, setForm] = useState<Form>({
+    name: {
+      value: pokemon.name,
+      error: '',
+      isValid: true,
+    },
+    hp: {
+      value: pokemon.hp,
+      error: '',
+      isValid: true,
+    },
+    cp: {
+      value: pokemon.cp,
+      error: '',
+      isValid: true,
+    },
+    types: {
+      value: pokemon.types,
+      error: '',
+      isValid: true,
+    },
+  });
+
   const types: string[] = [
-    'Plante', 'Feu', 'Eau', 'Insecte', 'Normal', 'Electrik',
-    'Poison', 'Fée', 'Vol', 'Combat', 'Psy'
+    'Plante',
+    'Feu',
+    'Eau',
+    'Insecte',
+    'Normal',
+    'Electrik',
+    'Poison',
+    'Fée',
+    'Vol',
+    'Combat',
+    'Psy',
   ];
-   
+
+  const hasType= (type:string):boolean =>{
+      return form.types.value.includes(type);
+  }
+
   return (
     <form>
       <div className="row">
         <div className="col s12 m8 offset-m2">
-          <div className="card hoverable"> 
+          <div className="card hoverable">
             <div className="card-image">
-              <img src={pokemon.picture} alt={pokemon.name} style={{width: '250px', margin: '0 auto'}}/>
+              <img
+                src={pokemon.picture}
+                alt={pokemon.name}
+                style={{ width: '250px', margin: '0 auto' }}
+              />
             </div>
             <div className="card-stacked">
               <div className="card-content">
                 {/* Pokemon name */}
                 <div className="form-group">
                   <label htmlFor="name">Nom</label>
-                  <input id="name" type="text" className="form-control"></input>
+                  <input id="name" type="text" className="form-control" defaultValue={form.name.value} ></input>
                 </div>
                 {/* Pokemon hp */}
                 <div className="form-group">
                   <label htmlFor="hp">Point de vie</label>
-                  <input id="hp" type="number" className="form-control"></input>
+                  <input id="hp" type="number" className="form-control" defaultValue={form.hp.value}></input>
                 </div>
                 {/* Pokemon cp */}
                 <div className="form-group">
                   <label htmlFor="cp">Dégâts</label>
-                  <input id="cp" type="number" className="form-control"></input>
+                  <input id="cp" type="number" className="form-control" defaultValue={form.cp.value}></input>
                 </div>
                 {/* Pokemon types */}
                 <div className="form-group">
                   <label>Types</label>
-                  {types.map(type => (
-                    <div key={type} style={{marginBottom: '10px'}}>
+                  {types.map((type) => (
+                    <div key={type} style={{ marginBottom: '10px' }}>
                       <label>
-                        <input id={type} type="checkbox" className="filled-in"></input>
+                        <input
+                          id={type}
+                          type="checkbox"
+                          className="filled-in"
+                          defaultChecked={hasType(type)}
+                          value={type}
+                        ></input>
                         <span>
-                          <p className={formatType(type)}>{ type }</p>
+                          <p className={formatType(type)}>{type}</p>
                         </span>
                       </label>
                     </div>
@@ -55,7 +113,9 @@ const PokemonForm: FunctionComponent<Props> = ({pokemon}) => {
               </div>
               <div className="card-action center">
                 {/* Submit button */}
-                <button type="submit" className="btn">Valider</button>
+                <button type="submit" className="btn">
+                  Valider
+                </button>
               </div>
             </div>
           </div>
@@ -64,5 +124,5 @@ const PokemonForm: FunctionComponent<Props> = ({pokemon}) => {
     </form>
   );
 };
-   
+
 export default PokemonForm;
